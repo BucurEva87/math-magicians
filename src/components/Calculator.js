06c42ea629c99a9907dc5026d05a647f2bb938a5
BucurEva87 <bucur.eva87@gmail.com>
2022-09-13 18:45:03 +0300

    Add event handling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,60 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import Display from './Display';
+import Button from './Button';
 import '../styles/Calculator.scss';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends Component {
+export default class Calculator extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      display: '0',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const buttonName = e.target.textContent;
+
+    this.setState((prevState) => ({ ...calculate(prevState, buttonName) }), this.displayOnScreen);
+  }
+
+  displayOnScreen() {
+    const { total, next } = this.state;
+
+    if (!next && !total) this.setState({ display: '0' });
+    else this.setState({ display: next ?? total });
+  }
+
   render() {
+    const { display } = this.state;
+
     return (
       <div id="calculator">
-        <div id="result">0</div>
-        <div id="allClear">AC</div>
-        <div id="switchSign">+/-</div>
-        <div id="percent">%</div>
-        <div id="divide" className="operation">÷</div>
-        <div id="btn7">7</div>
-        <div id="btn8">8</div>
-        <div id="btn9">9</div>
-        <div id="times" className="operation">x</div>
-        <div id="btn6">6</div>
-        <div id="btn5">5</div>
-        <div id="btn4">4</div>
-        <div id="minus" className="operation">-</div>
-        <div id="btn3">3</div>
-        <div id="btn2">2</div>
-        <div id="btn1">1</div>
-        <div id="plus" className="operation">+</div>
-        <div id="btn0">0</div>
-        <div id="dot">.</div>
-        <div id="equals" className="operation">=</div>
+        <Display>{display}</Display>
+        <Button id="allClear" handleClick={this.handleClick}>AC</Button>
+        <Button id="switchSign" handleClick={this.handleClick}>+/-</Button>
+        <Button id="percent" handleClick={this.handleClick}>%</Button>
+        <Button id="Buttonide" handleClick={this.handleClick} className="operation">÷</Button>
+        <Button id="btn7" handleClick={this.handleClick}>7</Button>
+        <Button id="btn8" handleClick={this.handleClick}>8</Button>
+        <Button id="btn9" handleClick={this.handleClick}>9</Button>
+        <Button id="times" handleClick={this.handleClick} className="operation">x</Button>
+        <Button id="btn6" handleClick={this.handleClick}>6</Button>
+        <Button id="btn5" handleClick={this.handleClick}>5</Button>
+        <Button id="btn4" handleClick={this.handleClick}>4</Button>
+        <Button id="minus" handleClick={this.handleClick} className="operation">-</Button>
+        <Button id="btn3" handleClick={this.handleClick}>3</Button>
+        <Button id="btn2" handleClick={this.handleClick}>2</Button>
+        <Button id="btn1" handleClick={this.handleClick}>1</Button>
+        <Button id="plus" handleClick={this.handleClick} className="operation">+</Button>
+        <Button id="btn0" handleClick={this.handleClick}>0</Button>
+        <Button id="dot" handleClick={this.handleClick}>.</Button>
+        <Button id="equals" handleClick={this.handleClick} className="operation">=</Button>
       </div>
     );
   }
